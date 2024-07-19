96695004f503e5aef65e2d1a7c3b5a1053d23bf2
Suresh Adhikari <aloneas47@gmail.com>
2024-07-19 16:35:46 -0400

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,34 @@
-const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-module.exports = (env, argv) => {
-  const isDevelopment = argv.mode === "development";
-
-  return {
-    entry: "./src/index.js", // entry point
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "bundle.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.html$/,
-          use: [
-            {
-              loader: "html-loader",
-              options: { minimize: !isDevelopment },
-            },
-          ],
-        },
-        {
-          test: /\.css$/,
-          use: ["style-loader", "css-loader"],
-        },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env", "@babel/preset-react"],
-            },
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
           },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-      ],
-    },
-    plugins: [
-      new HtmlWebPackPlugin({
-        template: "./public/index.html",
-        filename: "index.html",
-      }),
+      },
     ],
-    resolve: {
-      extensions: [".js", ".jsx"],
-    },
-    devServer: {
-      contentBase: path.join(__dirname, "dist"),
-      compress: true,
-      port: 3000,
-      open: true,
-    },
-  };
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
